7a4dd09e9f62c2294b723afc2d5dab44d1a6680f
lionleo0139 <lionleo0139@gmail.com>
2019-03-02 10:21:19 +0800

    refactor(location): improvement min stack with two stack

--- golang/stack/min_stack/main.go
+++ golang/stack/min_stack/main.go
@@ -2,12 +2,16 @@ package min_stack
 
 type MinStack struct {
 	data []int
+	min  []int
 	top  int
 }
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
-	return MinStack{nil, -1}
+	var stack MinStack
+	stack.min = append(stack.min, int(^uint(0)>>1))
+	stack.top = -1
+	return stack
 }
 
 func (this *MinStack) Push(x int) {
@@ -16,16 +20,24 @@ func (this *MinStack) Push(x int) {
 	} else {
 		this.top++
 	}
+
 	if this.top < len(this.data) {
 		this.data[this.top] = x
 	} else {
 		this.data = append(this.data, x)
+	}
+
+	if x <= this.min[len(this.min)-1] {
+		this.min = append(this.min, x)
 	}
 }
 
 func (this *MinStack) Pop() {
 	if this.top < 0 {
 		return
+	}
+	if this.data[this.top] == this.min[len(this.min)-1] {
+		this.min = this.min[:len(this.min)-1]
 	}
 	this.top--
 }
@@ -39,14 +51,5 @@ func (this *MinStack) Top() int {
 }
 
 func (this *MinStack) GetMin() int {
-	if this.top < 0 {
-		return 0
-	}
-	v := this.data[0]
-	for i := 0; i <= this.top; i++ {
-		if this.data[i] < v {
-			v = this.data[i]
-		}
-	}
-	return v
+	return this.min[len(this.min)-1]
 }
