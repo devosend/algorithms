62a17b483b13543d7ecfa1cc369cb50db0af4570
lionleo0139 <lionleo0139@gmail.com>
2019-03-10 16:56:58 +0800

    feat(location): 添加顺序队列

---
+++ golang/queue/array_queue/main.go
@@ -0,0 +1,32 @@
+package array_queue
+
+type ArrayQueue struct {
+	data     []int
+	capacity int
+	tail     int
+	head     int
+}
+
+// 初始化一个队列
+func newQueue(n int) ArrayQueue {
+	return ArrayQueue{make([]int, n), n, 0, 0}
+}
+
+//入队
+func (this *ArrayQueue) push(item int) {
+	if this.tail >= this.capacity {
+		return
+	}
+	this.data[this.tail] = item
+	this.tail += 1
+}
+
+//出队
+func (this *ArrayQueue) pop() int {
+	if this.head >= this.tail {
+		return ^int(^uint(0) >> 1)
+	}
+	val := this.data[this.head]
+	this.head += 1
+	return val
+}
