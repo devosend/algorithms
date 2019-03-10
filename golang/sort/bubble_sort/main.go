d89078d89ec2e5f387acb2163a3f3dacc690039a
lionleo0139 <lionleo0139@gmail.com>
2019-03-10 22:46:31 +0800

    feat(location): 添加冒泡排序

---
+++ golang/sort/bubble_sort/main.go
@@ -0,0 +1,22 @@
+package bubble_sort
+
+func BubbleSort(arr []int, n int) []int {
+	if n == 0 || n == 1 {
+		return arr
+	}
+	for i := 0; i < n; i++ {
+		flag := false
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				tmp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = tmp
+				flag = true
+			}
+		}
+		if !flag {
+			return arr
+		}
+	}
+	return arr
+}
